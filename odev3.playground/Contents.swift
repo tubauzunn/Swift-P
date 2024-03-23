e6055cf85e7c03f44d1ffd897612c54b7e4651c0
tuba <tubauzunnnn@gmail.com>
2024-03-23 17:41:24 +0300

    odev3

--- odev3.playground/Contents.swift
+++ odev3.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//  1. soru
+
+func printPlusMinusPattern(_ n: Int) {
+    var pattern = ""
+    for i in 1...n {
+        pattern += i % 2 == 0 ? "-" : "+"
+    }
+    print(pattern)
+}
+printPlusMinusPattern(5)
+
+
+// 2. soru
+
+func appendFiveToMax(_ number: Int) -> Int {
+    let maxDigit = String(number).max() ?? "0"
+    return Int(String(maxDigit) + "5") ?? 0
+}
+
+appendFiveToMax(30)
